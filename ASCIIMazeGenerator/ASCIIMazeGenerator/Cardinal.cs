5c7ca36a03bc0757950aebfc78c0b36aa8433202
Argocyte <35248818+Argocyte@users.noreply.github.com>
2020-02-20 01:42:44 +0000

    Add files via upload

---
+++ ASCIIMazeGenerator/ASCIIMazeGenerator/Cardinal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ASCIIMazeGenerator
+{
+    [Flags]
+    public enum Cardinal
+    {
+        NONE = 0,
+        EAST = 1 << 0,
+        NORTH = 1 << 1,
+        SOUTH = 1 << 2,
+        WEST = 1 << 3
+    }
+}
